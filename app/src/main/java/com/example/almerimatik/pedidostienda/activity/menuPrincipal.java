9a28321d6ac6e252b347435ee449cf1363d2ad60
Alejandro Almerimatik <alejandro.almerimatik@gmail.com>
2018-01-16 13:42:27 +0100

    Primer commit

---
+++ app/src/main/java/com/example/almerimatik/pedidostienda/activity/menuPrincipal.java
@@ -0,0 +1,25 @@
+package com.example.almerimatik.pedidostienda.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.almerimatik.pedidostienda.R;
+
+public class menuPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
+
+    public void abrirCatalogo(View view) {
+    }
+
+    public void abrirListas(View view) {
+    }
+
+    public void abrirHistorial(View view) {
+    }
+}
