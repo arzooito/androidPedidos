2eda5199beb734cca2cdd82a0fcd0272e3d98177
Alejandro Almerimatik <alejandro.almerimatik@gmail.com>
2018-01-18 13:24:52 +0100

    Agregado activity historial y modificada la activity base

---
+++ app/src/main/java/com/example/almerimatik/pedidostienda/activity/HistorialActivity.java
@@ -0,0 +1,20 @@
+package com.example.almerimatik.pedidostienda.activity;
+
+import android.os.Bundle;
+
+import com.example.almerimatik.pedidostienda.R;
+
+import static com.example.almerimatik.pedidostienda.constantes.Tipo.HISTORIAL;
+
+/**
+ * Created by Almerimatik on 18/01/2018.
+ */
+
+public class HistorialActivity extends ListadoBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityTipo = HISTORIAL;
+    }
+}
---
+++ app/src/main/java/com/example/almerimatik/pedidostienda/constantes/Tipo.java
@@ -0,0 +1,12 @@
+package com.example.almerimatik.pedidostienda.constantes;
+
+/**
+ * Created by Almerimatik on 18/01/2018.
+ */
+
+public class Tipo {
+
+    public static final int BASE = -1;
+    public static final int CATALOGO = 0;
+    public static final int HISTORIAL = 1;
+}
